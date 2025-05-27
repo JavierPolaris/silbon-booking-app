50a230332ff8b4c1cdf0ffa1a9aa6e19790aade0
jcantonSilbon <jcanton@silbon.es>
2025-05-27 09:29:39 +0200

    csxcscs

--- utils/getToken.js
+++ utils/getToken.js
@@ -2,12 +2,13 @@ import axios from 'axios';
 
 export async function getTimifyToken() {
   const clientId = process.env.TIMIFY_APP_ID;
-  const clientSecret = process.env.TIMIFY_APP_SECRET;
+const clientSecret = process.env.TIMIFY_APP_SECRET;
 
-  if (!clientId || !clientSecret) {
-    console.error("❌ Variables de entorno no definidas");
-    return null;
-  }
+
+ if (!clientId || !clientSecret) {
+  console.error("❌ Faltan variables TIMIFY_APP_ID o TIMIFY_APP_SECRET");
+  return null;
+}
 
   try {
     const response = await axios.post('https://api.timify.com/v1/auth/token', {
