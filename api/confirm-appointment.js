cbc6b53ed3493818826e536bb96466d058043d23
jcantonSilbon <jcanton@silbon.es>
2025-05-28 12:02:19 +0200

    dale

--- api/confirm-appointment.js
+++ api/confirm-appointment.js
@@ -20,7 +20,7 @@ export default async function handler(req, res) {
         lastName,
         email,
         phoneNumber
-    } = body;
+    } = req.body;
 
 
     if (!reservationId || !secret || !firstName || !lastName || !email || !phoneNumber) {
