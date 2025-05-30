638d6e0460293c13bb801d4befab18b5a45e5644
jcantonSilbon <jcanton@silbon.es>
2025-05-30 11:04:38 +0200

    up

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import BookingModal from './BookingModal.jsx'
+import Tiendas from './Tiendas.jsx'
 import './BookingModal.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Tiendas />
+    {/* Render the BookingModal component */}
     <BookingModal />
   </React.StrictMode>
 )
